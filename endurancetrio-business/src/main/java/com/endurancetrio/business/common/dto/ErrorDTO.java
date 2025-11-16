ee454345b687de29368a79098587e42a66f1c639
Ricardo do Canto <endurancecode@gmail.com>
2025-11-16 22:40:07 +0000

    [TRACKER] Add a base custom Exception class

---
+++ endurancetrio-business/src/main/java/com/endurancetrio/business/common/dto/ErrorDTO.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2025-2025 Ricardo do Canto
+ *
+ * This file is part of the EnduranceTrio Tracker project.
+ *
+ * Licensed under the Functional Software License (FSL), Version 1.1, ALv2 Future License
+ * (the "License");
+ *
+ * You may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://fsl.software/
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND WITHOUT WARRANTIES OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING WITHOUT LIMITATION WARRANTIES OF FITNESS FOR A PARTICULAR
+ * PURPOSE, MERCHANTABILITY, TITLE OR NON-INFRINGEMENT.
+ *
+ * IN NO EVENT WILL WE HAVE ANY LIABILITY TO YOU ARISING OUT OF OR RELATED TO THE
+ * SOFTWARE, INCLUDING INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ * EVEN IF WE HAVE BEEN INFORMED OF THEIR POSSIBILITY IN ADVANCE.
+ */
+
+package com.endurancetrio.business.common.dto;
+
+import com.endurancetrio.data.tracker.model.entity.TrackingData;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+/**
+ * The {@link ErrorDTO} class represents an error response with an error code and a descriptive
+ * message.
+ */
+public class ErrorDTO implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  private String error;
+  private String message;
+
+  public ErrorDTO(String error, String message) {
+    this.error = error;
+    this.message = message;
+  }
+
+  public String getError() {
+    return error;
+  }
+
+  public void setError(String error) {
+    this.error = error;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ErrorDTO that = (ErrorDTO) o;
+    return Objects.equals(error, that.error) && Objects.equals(message, that.message);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(error, message);
+  }
+
+  @Override
+  public String toString() {
+    return new StringJoiner(", ", TrackingData.class.getSimpleName() + "[", "]")
+        .add("error=" + error)
+        .add("message='" + message + "'")
+        .toString();
+  }
+}
---
+++ endurancetrio-business/src/main/java/com/endurancetrio/business/common/exception/base/EnduranceTrioException.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2025-2025 Ricardo do Canto
+ *
+ * This file is part of the EnduranceTrio Tracker project.
+ *
+ * Licensed under the Functional Software License (FSL), Version 1.1, ALv2 Future License
+ * (the "License");
+ *
+ * You may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://fsl.software/
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND WITHOUT WARRANTIES OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING WITHOUT LIMITATION WARRANTIES OF FITNESS FOR A PARTICULAR
+ * PURPOSE, MERCHANTABILITY, TITLE OR NON-INFRINGEMENT.
+ *
+ * IN NO EVENT WILL WE HAVE ANY LIABILITY TO YOU ARISING OUT OF OR RELATED TO THE
+ * SOFTWARE, INCLUDING INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ * EVEN IF WE HAVE BEEN INFORMED OF THEIR POSSIBILITY IN ADVANCE.
+ */
+
+package com.endurancetrio.business.common.exception.base;
+
+import com.endurancetrio.business.common.dto.ErrorDTO;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The {@link EnduranceTrioException} class serves as the base exception for all custom exceptions
+ * in the EnduranceTrio Tracker project. It extends the standard Java {@link Exception} class and
+ * includes additional fields for an error code and a list of detailed error information.
+ */
+public abstract class EnduranceTrioException extends Exception {
+
+  private final int code;
+  private final List<ErrorDTO> errors = new ArrayList<>();
+
+  protected EnduranceTrioException(final int code, final String message) {
+    super(message);
+    this.code = code;
+  }
+
+  protected EnduranceTrioException(final int code, final String message, final List<ErrorDTO> errors) {
+    super(message);
+    this.code = code;
+    if (errors != null) {
+      this.errors.addAll(errors);
+    }
+  }
+
+  public int getCode() {
+    return code;
+  }
+
+  public List<ErrorDTO> getErrors() {
+    return errors;
+  }
+}
